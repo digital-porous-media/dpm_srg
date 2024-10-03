26a37d8bd05444657aa7d961f2a8bb318762a757
bcchang <bcchang@utexas.edu>
2024-10-03 17:19:14 -0500

    Add setup.py file for easy pip install

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, Extension
+import pybind11
+from pybind11.setup_helpers import Pybind11Extension
+
+# Define your extension module
+ext_modules = [
+    Pybind11Extension(
+        'dpm_srg',  # Name of the module
+        ['dpm_srg/wrapper.cpp'],
+        include_dirs=[pybind11.get_include(), 'include'],
+        language='c++'
+    ),
+]
+
+setup(
+    name='dpm_srg',
+    version='0.1.0',
+    author='Digital Porous Media',
+    author_email='bcchang@utexas.edu',
+    description='Seeded Region Growing Segmentation',
+    ext_modules=ext_modules,
+    zip_safe=False,
+)
