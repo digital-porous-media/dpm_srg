3af353ef74c744f60f15b0945740e39b7b19f71a
bcchang <bcchang@utexas.edu>
2024-10-10 10:14:16 -0500

    Move long description to pyproject

--- setup.py
+++ setup.py
@@ -2,15 +2,12 @@ from setuptools import setup, find_packages
 import pybind11
 from pybind11.setup_helpers import Pybind11Extension
 import sys
-from pathlib import Path
 
 if sys.platform == "win32":
     extra_compile_args = ["/std:c++17"]
 else:
     extra_compile_args = ["-std=c++17"]
 
-this_directory = Path(__file__).parent
-long_description = (this_directory/"README.md").read_text()
 
 # Define your extension module
 ext_modules = [
@@ -29,8 +26,6 @@ setup(
     author='Digital Porous Media',
     author_email='bcchang@utexas.edu',
     description='Seeded Region Growing Segmentation',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     ext_modules=ext_modules,
     zip_safe=False,
     packages=find_packages(exclude=["include"])
